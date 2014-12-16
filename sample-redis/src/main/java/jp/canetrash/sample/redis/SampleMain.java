7f6d5ae39e61a46b233c568b1819e5a5a7c040da
tfunato <tfunato@healthcare.ne.jp>
2014-12-16 15:29:41 +0900

    redis sample modify

--- sample-redis/src/main/java/jp/canetrash/sample/redis/SampleMain.java
+++ sample-redis/src/main/java/jp/canetrash/sample/redis/SampleMain.java
@@ -1,15 +1,26 @@
 package jp.canetrash.sample.redis;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class SampleMain {
 	
 	public static void main(String[] args) {
 		
-		Jedis jedis = new Jedis("192.168.0.204", 6379);
+		JedisPool pool = new JedisPool(new JedisPoolConfig(), "localhost", 6379, 10);
+		Jedis jedis = pool.getResource();
+
 		jedis.set("foo", "bar");
-		String value = jedis.get("foo");
-		System.out.println(value);
+		String foobar = jedis.get("foo");
+		jedis.expire("foo", 10);
+		
+		System.out.println(foobar);
+		
+		//jedis.del("foo");
+
+		pool.returnResource(jedis);
+		pool.destroy();
 	}
 
 }
